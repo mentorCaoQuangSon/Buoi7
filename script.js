b860479b97c5459ddd36d1ccbf9002da98dc5d8b
sonkoi24112 <soncao24115@myschool.host>
2022-01-04 18:56:38 +0700

    Update Buoi 7

---
+++ script.js
@@ -0,0 +1,32 @@
+//BT1 TINH TONG 
+// let diemToan = prompt("Mời nhập điểm Toán");
+// let diemVan = prompt("Mời nhập điểm Văn");
+// let diemAnh = prompt("Mời nhập điểm Anh");
+// // Ép kiểu từ String sang Number
+// let diemTrungBinh = (Number(diemToan) + Number(diemVan) + Number(diemAnh)) / 3;
+// // Lấy chữ số thập phân đầu tiên sau dấu phẩy
+// console.log(diemTrungBinh.toFixed(1))
+// let firstName =prompt("Nhap ten cua ban ?");
+// let lastName = prompt("Nhap ho cua ban ?")
+
+//BT2
+//let name = "Ilya";
+// alert(`hello $ { 1 }`);
+//alert(`hello ${ 1 }`)
+//alert(` hello ${"name"} `); // ?
+//alert(` hello ${name} `); // ?
+//BT3
+//let _name = "Cao Quang Son"
+let rep = `Baby Em như bông hoa
+Nhưng người hái đâu phải ta
+Em vội mang bao câu ca
+Trôi về nơi xa xa xa`
+    //length
+    //ThayThe
+    //find string
+    //cut string(substr, substring)
+    //gop mang
+    //Sokitu trong chuoi
+
+//console.log(_name.replace(/\s/g, "").length);
+console.log(rep.replace(/Em/g, "Trang"));
